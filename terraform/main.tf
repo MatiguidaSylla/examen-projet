2b020966e153711c7159c8ac2ec2cc5fb62fe501
MatiguidaSylla <matiguidasylla794@gmail.com>
2025-07-13 11:41:22 +0000

    Update main.tf
    correction de main tf avec les groupes de securite

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,6 +3,13 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
 }
+
+# Récupère automatiquement le VPC par défaut
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Récupère l'AMI Ubuntu la plus récente
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"] # Canonical (Ubuntu)
@@ -23,15 +30,60 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+# Groupe de sécurité avec accès SSH, Grafana, Prometheus
+resource "aws_security_group" "monitoring_sg" {
+  name        = "monitoring-sg"
+  description = "Allow SSH, Grafana and Prometheus"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # 
+  }
+
+  ingress {
+    description = "Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "monitoring-security-group"
+  }
+}
+
+# Instance EC2
 resource "aws_instance" "monitoring_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  key_name      = var.key_name
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.monitoring_sg.id]
 
   tags = {
     Name = "monitoring-server"
   }
 
+  # Génère le fichier hosts pour Ansible automatiquement
   provisioner "local-exec" {
     command = "echo '[monitoring]\n${self.public_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/${var.key_file}.pem' > ../ansible/hosts"
   }
