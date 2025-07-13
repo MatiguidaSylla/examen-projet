97c8d3d940cd8ab1204769b9ce1ab998e52017f4
MatiguidaSylla <matiguidasylla794@gmail.com>
2025-07-13 14:16:06 +0000

    Update main.tf
    correction

--- terraform/main.tf
+++ terraform/main.tf
@@ -87,8 +87,3 @@ resource "aws_instance" "monitoring_server" {
     EOT
   }
 }
-
-# Output pour récupérer l’IP publique
-output "instance_ip" {
-  value = aws_instance.monitoring_server.public_ip
-}
